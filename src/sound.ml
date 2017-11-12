0014df219e13e54bee297da9bdd4d1043abca78f
Daniel Weber <dantech88@yahoo.com>
2017-11-12 18:02:07 -0500

    finished sound module

--- src/sound.ml
+++ src/sound.ml
@@ -1,3 +1,5 @@
+open Bigarray
+
 type sound = {
   pitches: (int, Bigarray.int16_signed_elt) Tsdl.Sdl.bigarray list;
   looping: bool;
@@ -5,6 +7,80 @@ type sound = {
   groups: int list;
   quantization: int;
   mutable playing: bool;
-  mutable current_pitch: int;
+  mutable pitch_index: int;
+  mutable current_pitch: (int, Bigarray.int16_signed_elt) Tsdl.Sdl.bigarray;
+  mutable current_pitch_len: int;
   mutable buffer_index: int;
 }
+
+(* The options for a sound *)
+type sound_options = {
+  loop: bool;
+  hold_to_play: bool;
+  groups: int list;
+  quantization: int; 
+}
+
+let create files options = 
+  let load_next_pitch file lst =
+    match lst with
+    | None -> None
+    | Some l ->
+      (match Tsdl_wrapper.load_wav file with
+      | None ->  print_endline ("Unable to load "^file); None
+      | Some arr -> Some (arr::l))
+  in match List.fold_right load_next_pitch files (Some []) with
+  | None -> None
+  | Some pitches ->
+    if List.length pitches = 0 then
+      None
+    else
+    let current_pitch = List.hd pitches in
+    let current_pitch_len = Array1.dim current_pitch in
+    Some {
+      pitches = pitches;
+      looping = options.loop;
+      hold_to_play = options.hold_to_play;
+      groups = options.groups;
+      quantization = options.quantization;
+      playing = false;
+      pitch_index = 0;
+      current_pitch = current_pitch;
+      current_pitch_len = current_pitch_len;
+      buffer_index = 0;
+    }
+
+let play_sound sound =
+  sound.playing <- true
+
+let pause_sound sound =
+  sound.playing <- false
+
+let stop_sound sound =
+  sound.playing <- false;
+  sound.buffer_index <- 0
+
+let next_pitch sound = 
+  let next_index =
+    if sound.pitch_index == List.length sound.pitches then
+      0
+    else
+      sound.pitch_index + 1
+  in sound.pitch_index <- next_index;
+  sound.current_pitch <- List.nth sound.pitches sound.pitch_index;
+  sound.current_pitch_len <- Array1.dim sound.current_pitch
+
+let get_next_values sound =
+  if sound.playing then
+    if sound.buffer_index + 1 >= sound.current_pitch_len then
+      let _ = sound.playing <- false in
+      (0,0)
+    else
+      let l = sound.current_pitch.{sound.buffer_index} in
+      let r = sound.current_pitch.{sound.buffer_index+1} in
+      let _ = sound.buffer_index <- sound.buffer_index + 2 in
+      (l,r)
+  else
+    (0,0)
+
+let is_playing sound = sound.playing
