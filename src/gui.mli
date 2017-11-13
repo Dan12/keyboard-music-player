13967cfa075de90a5034a04f5d88006014eefbac
David Zhang <davidzhang@dhcp-mcfad-3806.redrover.cornell.edu>
2017-11-12 19:22:27 -0500

    update

--- src/gui.mli
+++ src/gui.mli
@@ -1,3 +1,7 @@
 (* This module will contain a draw call called by the Tsdl loop and
  * receive input events from the sound manager to update the draw state.
  *)
+
+ (*draw line
+  draw rectangle
+  draw text*)
