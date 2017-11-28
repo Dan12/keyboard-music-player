67270868c35cea2b8db750ca791529c40ef26f0d
sjb274 <sjb274@cornell.edu>
2017-11-27 22:58:43 -0500

    added reactive soundpacks (no labels)

--- src/gui.ml
+++ src/gui.ml
@@ -6,6 +6,7 @@ open Keyboard
 let fonts = Hashtbl.create 16
 
 let percent_key_padding = 10
+let arrow_width_height_ratio = 2
 
 let background_color = Sdl.Color.create 255 255 255 255
 let keyboard_text_color = Sdl.Color.create 0 0 0 255
@@ -70,7 +71,7 @@ let draw_key_text r x y w h font = function
   | Empty -> ()
 
 
-let draw_key r x y w h font key_visual key_state =
+let draw_key r x y w h key_state =
   (match key_state with
    | KSDown -> set_color r keyboard_pressed_color;
      let rect = Sdl.Rect.create x y w h in
@@ -80,24 +81,53 @@ let draw_key r x y w h font key_visual key_state =
   set_color r keyboard_border_color;
   let rect = Sdl.Rect.create x y w h in
   let _ = Sdl.render_draw_rect r (Some rect) in
-  draw_key_text r x y w h font key_visual
+  ()
 
 (*
  * Assumes the key list has length of [row] * [col]
  *)
-let draw_keyboard renderer keyboard_layout keyboard x y w row col =
-  let offset = w / col in
+let draw_keyboard renderer keyboard_layout keyboard x y w rows cols =
+  let offset = w / cols in
   let key_size = (100 - percent_key_padding) * offset / 100 in
   let font = get_font (7 * key_size / 10) in
-  for r = 0 to row - 1 do
-    for c = 0 to col - 1 do
+  for r = 0 to rows - 1 do
+    for c = 0 to cols - 1 do
       let key_visual = Keyboard_layout.get_visual (r, c) keyboard_layout in
-      let key_state = Keyboard.get_state (r, c) keyboard in
+      let key_state = Keyboard.get_state_key (r, c) keyboard in
       let curr_x = c * offset + x in
       let curr_y = r * offset + y in
-      draw_key renderer curr_x curr_y key_size key_size font key_visual key_state;
+      draw_key renderer curr_x curr_y key_size key_size key_state;
+      draw_key_text renderer curr_x curr_y key_size key_size font key_visual
     done;
-  done
+  done;
+  offset * rows
+
+  (*
+   * Assumes the key list has length of [row] * [col]
+   *)
+let draw_arrows renderer keyboard x y w =
+  let x_offset = w / 3 in
+  let y_offset = x_offset / arrow_width_height_ratio in
+  let w_key = (100 - percent_key_padding) * x_offset / 100 in
+  let h_key = (100 - percent_key_padding) * y_offset / 100 in
+
+  (* draw left *)
+  let left_state = Keyboard.get_state_arrow 0 keyboard in
+  let up_state = Keyboard.get_state_arrow 1 keyboard in
+  let down_state = Keyboard.get_state_arrow 2 keyboard in
+  let right_state = Keyboard.get_state_arrow 3 keyboard in
+
+  draw_key renderer x y w_key h_key left_state;
+
+
+  (* draw down *)
+  draw_key renderer (x + x_offset) y w_key h_key down_state;
+
+  (* draw up *)
+  draw_key renderer (x + x_offset) (y - y_offset) w_key h_key up_state;
+
+  (* draw right *)
+  draw_key renderer (x + 2 * x_offset) y w_key h_key right_state
 
 let clear r =
   set_color r background_color;
@@ -111,5 +141,18 @@ let present r =
 
 let draw keyboard_layout keyboard r =
   clear r;
-  draw_keyboard r keyboard_layout keyboard 20 20 1200 4 12;
+  let init_x = 20 in
+  let init_y = 20 in
+
+  let keyboard_w = 1200 in
+  let keyboard_rows = Keyboard_layout.get_rows keyboard_layout in
+  let keyboard_cols = Keyboard_layout.get_cols keyboard_layout in
+  let keyboard_h = draw_keyboard r keyboard_layout keyboard
+      init_x init_y keyboard_w keyboard_rows keyboard_cols in
+  print_int keyboard_h;
+
+  let arrows_w = keyboard_w / 6 in
+  let arrows_x = init_x + keyboard_w / 2 - arrows_w / 2 in
+  let arrows_y = 9 * keyboard_h / 8 + init_y in
+  draw_arrows r keyboard arrows_x arrows_y arrows_w;
   present r
