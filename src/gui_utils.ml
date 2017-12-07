81214b21b15edb80dd51b27dda6adb553ce1d34d
sjb274 <sjb274@cornell.edu>
2017-12-07 12:47:50 -0500

    commented gui_utils

--- src/gui_utils.ml
+++ src/gui_utils.ml
@@ -3,12 +3,15 @@ open Tsdl_ttf
 open Keyboard
 open Keyboard_layout
 
+(* binds a Tsdl option with it's returning object, should never fail *)
 let (>>=) o f = match o with
   | Error (`Msg e) -> failwith (Printf.sprintf "Error %s" e)
   | Ok a -> f a
 
+(* cache used fonts so we don't need to reload every time they are used *)
 let fonts = Hashtbl.create 16
 
+(* returns a font with the specified font size *)
 let get_font size =
   match Hashtbl.find_opt fonts size with
   | None -> Ttf.open_font "resources/agane.ttf" size >>= fun font ->
@@ -17,17 +20,17 @@ let get_font size =
   | Some font -> font
 
 
-
+(* set the currently used color in the renderer to [color] *)
 let set_color r color =
   let red = Sdl.Color.r color in
   let green = Sdl.Color.g color in
   let blue = Sdl.Color.b color in
   let alpha = Sdl.Color.a color in
-  let _ = Sdl.set_render_draw_color r red green blue alpha in
-  ()
+  Sdl.set_render_draw_color r red green blue alpha |> ignore
 
 
-
+(* draw the requested [str] text with the specified [color] and font size [size]
+ * centered at the point ([x], [y])*)
 let draw_text r x y font_size color str =
   let font = get_font font_size in
   (* defines the bounds of the font *)
@@ -37,9 +40,11 @@ let draw_text r x y font_size color str =
   Ttf.render_text_solid font str color >>= fun (sface) ->
   Sdl.create_texture_from_surface r sface >>= fun (font_texture) ->
   let _ = Sdl.render_copy ~dst:text_rect r font_texture in
-  let () = Sdl.free_surface sface in
+  Sdl.free_surface sface;
   Sdl.destroy_texture font_texture
 
+(* draw the visualization of the shift key in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_shift r x y w h =
   let _ = Sdl.render_draw_line r x (y + h / 2) (x + w / 2) y in
   let _ = Sdl.render_draw_line r (x + w / 2) y (x + w) (y + h / 2) in
@@ -47,9 +52,10 @@ let draw_shift r x y w h =
   let _ = Sdl.render_draw_line r (x + 3 * w / 4) (y + h / 2) (x + 3 * w / 4) (y + h) in
   let _ = Sdl.render_draw_line r (x + 3 * w / 4) (y + h) (x + w / 4) (y + h) in
   let _ = Sdl.render_draw_line r (x + w / 4) (y + h) (x + w / 4) (y + h / 2) in
-  let _ = Sdl.render_draw_line r (x + w / 4) (y + h / 2) x (y + h / 2) in
-  ()
+  Sdl.render_draw_line r (x + w / 4) (y + h / 2) x (y + h / 2) |> ignore
 
+(* draw the visualization of the enter key in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_enter r x y w h =
   let _ = Sdl.render_draw_line r x (y + 2 * h / 3) (x + w / 4) (y + h / 3) in
   let _ = Sdl.render_draw_line r (x + w / 4) (y + h / 3) (x + w / 4) (y + h / 2) in
@@ -59,42 +65,48 @@ let draw_enter r x y w h =
   let _ = Sdl.render_draw_line r (x + w) y (x + w) (y + 5 * h / 6) in
   let _ = Sdl.render_draw_line r (x + w) (y + 5 * h / 6) (x + w / 4) (y + 5 * h / 6) in
   let _ = Sdl.render_draw_line r (x + w / 4) (y + 5 * h / 6) (x + w / 4) (y + h) in
-  let _ = Sdl.render_draw_line r (x + w / 4) (y + h) x (y + 2 * h / 3) in
-  ()
+  Sdl.render_draw_line r (x + w / 4) (y + h) x (y + 2 * h / 3) |> ignore
 
+(* draw the visualization of the left arrow key in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_left r x y w h =
   let _ = Sdl.render_draw_line r (x + w / 4) (y + h / 2) (x + 3 * w / 4) (y + h / 2) in
   let _ = Sdl.render_draw_line r (x + w / 4) (y + h / 2) (x + w / 2) (y + 3 * h / 4) in
-  let _ = Sdl.render_draw_line r (x + w / 4) (y + h / 2) (x + w / 2) (y + h / 4) in
-  ()
+  Sdl.render_draw_line r (x + w / 4) (y + h / 2) (x + w / 2) (y + h / 4) |> ignore
 
+(* draw the visualization of the down arrow key in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_down r x y w h =
   let _ = Sdl.render_draw_line r (x + w / 2) (y + 3 * h / 4) (x + w / 2) (y + h / 4) in
   let _ = Sdl.render_draw_line r (x + w / 2) (y + 3 * h / 4) (x + w / 3) (y + h / 2) in
-  let _ = Sdl.render_draw_line r (x + w / 2) (y + 3 * h / 4) (x + 2 * w / 3) (y + h / 2) in
-  ()
+  Sdl.render_draw_line r (x + w / 2) (y + 3 * h / 4) (x + 2 * w / 3) (y + h / 2) |> ignore
 
+(* draw the visualization of the up arrow key in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_up r x y w h =
   let _ = Sdl.render_draw_line r (x + w / 2) (y + h / 4) (x + w / 2) (y + 3 * h / 4) in
   let _ = Sdl.render_draw_line r (x + w / 2) (y + h / 4) (x + w / 3) (y + h / 2) in
-  let _ = Sdl.render_draw_line r (x + w / 2) (y + h / 4) (x + 2 * w / 3) (y + h / 2) in
-  ()
+  Sdl.render_draw_line r (x + w / 2) (y + h / 4) (x + 2 * w / 3) (y + h / 2) |> ignore
 
+(* draw the visualization of the right arrow key in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_right r x y w h =
   let _ = Sdl.render_draw_line r (x + w / 4) (y + h / 2) (x + 3 * w / 4) (y + h / 2) in
   let _ = Sdl.render_draw_line r (x + 3 * w / 4) (y + h / 2) (x + w / 2) (y + 3 * h / 4) in
-  let _ = Sdl.render_draw_line r (x + 3 * w / 4) (y + h / 2) (x + w / 2) (y + h / 4) in
-  ()
+  Sdl.render_draw_line r (x + 3 * w / 4) (y + h / 2) (x + w / 2) (y + h / 4) |> ignore
 
+(* draw the visualization of the play triangle in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_play r x y w h =
   let w_padding = w / 5 in
   let h_padding = h / 5 in
 
   let _ = Sdl.render_draw_line r (x+w_padding) (y+h_padding) (x+w-w_padding) (y+h/2) in
   let _ = Sdl.render_draw_line r (x+w_padding) (y+h-h_padding) (x+w-w_padding) (y+h/2) in
-  let _ = Sdl.render_draw_line r (x+w_padding) (y+h_padding) (x+w_padding) (y+h-h_padding) in
-  ()
+  Sdl.render_draw_line r (x+w_padding) (y+h_padding) (x+w_padding) (y+h-h_padding) |> ignore
 
+(* draw the visualization of the pause bars in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_pause r x y w h =
   let w_padding = w / 5 in
   let h_padding = h / 5 in
@@ -103,18 +115,20 @@ let draw_pause r x y w h =
   let rect_height = h - 2 * h_padding in
   let left_rect = Sdl.Rect.create (x+w_padding) (y+h_padding) rect_width rect_height in
   let right_rect = Sdl.Rect.create (x+rect_width+2*w_padding) (y+h_padding) rect_width rect_height in
-  let _ = Sdl.render_draw_rects r [left_rect;right_rect] in
-  ()
+  Sdl.render_draw_rects r [left_rect;right_rect] |> ignore
 
+(* draw the visualization of the stop square in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
 let draw_stop r x y w h =
   let w_padding = w / 5 in
   let h_padding = h / 5 in
 
   let rect = Sdl.Rect.create (x+w_padding) (y+h_padding) (w-w_padding*2) (h-h_padding*2) in
-  let _ = Sdl.render_draw_rect r (Some rect) in
-  ()
+  Sdl.render_draw_rect r (Some rect) |> ignore
 
 
+(* draw a key's text in the bounding box ([x], [y]) - ([x] + [w], [y] + [h]).
+ * The text is based on the key type *)
 let draw_key_text r x y w h font_size text_color = function
   | String s -> draw_text r (x + w / 2) (y + h / 2) font_size text_color s
   | Shift -> draw_shift r (x  + w / 4) (y + h / 4) (w / 2) (h / 2)
@@ -123,21 +137,17 @@ let draw_key_text r x y w h font_size text_color = function
 
 
 
-let draw_key_to_rect r x y w h down_color up_color boarder_color key_state =
+(* draw a pressed or unpressed key in the bounding box
+ * ([x], [y]) - ([x] + [w], [y] + [h]) *)
+let draw_key r x y w h down_color up_color boarder_color key_state =
   (match key_state with
    | KSDown -> set_color r down_color;
      let rect = Sdl.Rect.create x y w h in
-     let _ = Sdl.render_fill_rect r (Some rect) in
-     ()
+     Sdl.render_fill_rect r (Some rect) |> ignore
    | _ -> set_color r up_color;
      let rect = Sdl.Rect.create x y w h in
-     let _ = Sdl.render_fill_rect r (Some rect) in
-     ());
+     Sdl.render_fill_rect r (Some rect) |> ignore);
 
   set_color r boarder_color;
   let rect = Sdl.Rect.create x y w h in
-  let _ = Sdl.render_draw_rect r (Some rect) in
-  rect
-
-let draw_key r x y w h down_color up_color boarder_color key_state =
-  draw_key_to_rect r x y w h down_color up_color boarder_color key_state |> ignore
+  Sdl.render_draw_rect r (Some rect) |> ignore
